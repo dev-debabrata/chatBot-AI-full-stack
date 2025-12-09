694e2a510ec25e760b02e7749d3825909245b367
Debabrata Das <debabratadas711@gmail.com>
2025-12-09 15:51:12 +0530

    09-12-2025

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import ChatBox from './components/ChatBox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <div className=''>
+        <div className=' flex h-screen w-screen'>
+          <Sidebar />
+          <Routes>
+            <Route path='/' element={<ChatBox />} />
+            <Route path='/credits' element={<Credits />} />
+            <Route path='/community' element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+
+    </>
   )
 }
 
