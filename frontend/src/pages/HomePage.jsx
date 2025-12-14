342b42fe74e7092438db4ec13b1463b5b8fcf65a
Debabrata Das <debabratadas711@gmail.com>
2025-12-14 23:01:02 +0530

    14-12-2025

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Search from "../components/Search";
 import Login from "../components/auth/Login";
 import SignUp from "../components/auth/SignUp";
 import { Link } from "react-router-dom";
-import Logo from "/logo.png"
+import HomeLogo from "/home.png"
 import Navbar from "../components/layout/Navbar";
 
 const HomePage = () => {
@@ -15,22 +15,29 @@ const HomePage = () => {
 
             <Navbar />
 
-            {/* Main content */}
-            <main className="flex-1 flex flex-col items-center justify-center px-4">
-                {/* Heading */}
-                <h1 className="text-3xl md:text-4xl font-semibold text-center text-gray-900 mb-10">
-                    What can I help with?
-                </h1>
 
-                {/* Chat/Search box */}
+            <main className="flex-1 flex flex-col items-center justify-center px-4">
+
+                <div className=" flex flex-col justify-center items-center gap-8">
+                    <img src={HomeLogo} alt="" className=" w-[250px] h-[250px] rounded-full" />
+                    <h1 className="text-3xl md:text-4xl font-semibold text-center text-gray-900 mb-10">
+                        {/* What can I help with? */}
+                        What can I help you with today?
+                    </h1>
+                </div>
+
+
                 {/* <Search /> */}
 
                 {/* Footer text like ChatGPT */}
-                <p className="mt-10 text-[11px] md:text-xs text-gray-500 text-center max-w-2xl">
+
+            </main>
+            <div className=" flex justify-center items-center mb-4">
+                <p className="mt-10 text-sm text-gray-500 text-center max-w-2xl">
                     By messaging ChatBot, an AI assistant, you agree to our Terms and have
                     read our Privacy Policy.
                 </p>
-            </main>
+            </div>
 
         </div>
     );
